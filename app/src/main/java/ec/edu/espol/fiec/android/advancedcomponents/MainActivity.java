ba5a9167cefe4d16ef7f566293176cf0520ec68e
Miguel Sanchez <mangelsr@live.com>
2019-11-01 23:21:18 -0500

    Se agrego la libreria a usar para el menu con fab, adicionalmente se agregaron los recursos de iconos

--- app/src/main/java/ec/edu/espol/fiec/android/advancedcomponents/MainActivity.java
+++ app/src/main/java/ec/edu/espol/fiec/android/advancedcomponents/MainActivity.java
@@ -3,6 +3,10 @@ package ec.edu.espol.fiec.android.advancedcomponents;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.leinardi.android.speeddial.SpeedDialActionItem;
+import com.leinardi.android.speeddial.SpeedDialView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final SpeedDialView speedDialView = findViewById(R.id.speedDial);
+
+        SpeedDialActionItem mapItem = new SpeedDialActionItem.Builder(R.id.fab_map,
+                R.drawable.ic_map).create();
+        speedDialView.addActionItem(mapItem);
+
+        speedDialView.setOnActionSelectedListener(new SpeedDialView.OnActionSelectedListener() {
+            @Override
+            public boolean onActionSelected(SpeedDialActionItem actionItem) {
+                switch (actionItem.getId()) {
+                    case R.id.fab_map:
+                        Toast.makeText(getApplicationContext(), "Se apachurro...",
+                                Toast.LENGTH_SHORT).show();
+                        speedDialView.close();
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
