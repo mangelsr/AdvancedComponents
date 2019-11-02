a4c0ffdec348dea4a59324e223592461f2d5973b
Calos Astudillo <carluast@espol.edu.ec>
2019-11-02 15:04:16 -0500

    Eliminar mi rama y pondre esta que es nueva

--- app/src/main/java/ec/edu/espol/fiec/android/advancedcomponents/MainActivity.java
+++ app/src/main/java/ec/edu/espol/fiec/android/advancedcomponents/MainActivity.java
@@ -2,13 +2,29 @@ package ec.edu.espol.fiec.android.advancedcomponents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button siguiente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        siguiente=(Button)findViewById(R.id.button);
+
+        siguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent siguiente=new Intent(MainActivity.this, button_menu.class);
+                startActivity(siguiente);
+            }
+        });
     }
 }
