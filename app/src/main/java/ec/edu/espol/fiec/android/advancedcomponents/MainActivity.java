55ca776d051ea122ad62c1413c5a5d69d949b2df
Dayana <dtgarzon@espol.edu.ec>
2019-11-03 13:10:52 -0500

    Experimento de calendario con eventos

--- app/src/main/java/ec/edu/espol/fiec/android/advancedcomponents/MainActivity.java
+++ app/src/main/java/ec/edu/espol/fiec/android/advancedcomponents/MainActivity.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    CustomCalendarView customCalendarView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        customCalendarView = (CustomCalendarView)findViewById(R.id.custom_calender_view);
     }
 }
