6810eaf7601e7ed0601ae9f6326d9e04eab34873
Miguel Sanchez <mangelsr@live.com>
2019-11-03 13:09:55 -0500

    Se agrego el metodo de navegacion en el menu

--- app/src/main/java/ec/edu/espol/fiec/android/advancedcomponents/MenuActivity.java
+++ app/src/main/java/ec/edu/espol/fiec/android/advancedcomponents/MenuActivity.java
@@ -1,14 +1,41 @@
 package ec.edu.espol.fiec.android.advancedcomponents;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.getbase.floatingactionbutton.FloatingActionsMenu;
+
 public class MenuActivity extends AppCompatActivity {
+
+    FloatingActionsMenu floatingActionsMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        floatingActionsMenu = findViewById(R.id.btnMenu);
+    }
+
+    public void navigate(View v) {
+        Intent intent = null;
+        switch (v.getId()) {
+            case R.id.btnMap:
+              intent = new Intent(MenuActivity.this, MapActivity.class);
+              break;
+//            case R.id.btnCalendar:
+//                intent = new Intent(MenuActivity.this, MapActivity.class);
+//                break;
+            case R.id.btnVideo:
+                intent = new Intent(MenuActivity.this, VideoActivity.class);
+                break;
+            case R.id.btnStatistics:
+                intent = new Intent(MenuActivity.this, GraphicActivity.class);
+                break;
+        }
+        if (intent != null) startActivity(intent);
+        floatingActionsMenu.collapse();
     }
 }
